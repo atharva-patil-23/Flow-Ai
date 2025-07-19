22b1c8eb387309c67965e27e6d751a76c23fd687
Atharva Patil <anpatil.1223@gmail.com>
2025-07-19 22:30:50 +0530

    initialize server with Express, Mongoose, and basic configuration

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import "dotenv/config"
+
+const app = express()
+
+app.use(express.json())
+app.use(cors());
+
+app.get('/', (req,res) => {
+    res.json({
+        message:"hello atharva"
+    })
+})
+
+const PORT = process.env.PORT || 7070;
+
+app.listen(PORT , () => {
+    console.log(`server is listening at PORT:${PORT} `)
+})

720ad695e49b0870328e585ea89cc7dd8973e553
Atharva Patil <anpatil.1223@gmail.com>
2025-07-19 22:58:17 +0530

    add User model with schema validation and preferences

---
+++ server/models/User.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    name:{
+        type: String,
+        required:true,
+        trim:true,
+        maxlength:[50, 'Name cannot exceed 50 characters']
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+    },
+    password: {
+        type:String,
+        required:true,
+        minlength:[6,'Password must be atleast 6 characters']
+    },
+    preferences:{
+        aiSuggestion:{
+            type:Boolean,
+            default:false
+        },
+        notifications:{
+            type:Boolean,
+            default:true
+        }
+    }
+
+
+},{timestamps:true})
+
+
+const User = mongoose.model("User", userSchema)
+
+export default User;

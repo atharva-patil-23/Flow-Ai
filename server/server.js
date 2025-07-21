ba8abd49225032ace2cf05f271502952b9e21688
Atharva Patil <anpatil.1223@gmail.com>
2025-07-22 01:07:08 +0530

    enhance server configuration with cookie-parser and improved request handling limits

--- server/server.js
+++ server/server.js
@@ -3,12 +3,20 @@ import mongoose from "mongoose"
 import cors from "cors"
 import "dotenv/config"
 import connectDB from "./config/db.js"
+import cookieParser from "cookie-parser"
 
 connectDB();
 const app = express()
 
-app.use(express.json())
-app.use(cors());
+app.use(express.json({limit:"20kb"}))
+app.use(express.urlencoded({extended:true,limit:"20kb"}))
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}));
 
 app.get('/', (req,res) => {
     res.json({

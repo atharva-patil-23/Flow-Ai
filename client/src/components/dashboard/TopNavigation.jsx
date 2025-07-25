4c8ecbf460c73e760f82d993e4911cf86ecd25c7
Atharva Patil <anpatil.1223@gmail.com>
2025-07-25 19:53:06 +0530

    add Sidebar component and comment out notification badge in TopNavigation

--- client/src/components/dashboard/TopNavigation.jsx
+++ client/src/components/dashboard/TopNavigation.jsx
@@ -58,9 +58,9 @@ const TopNavigation = ({
           <button className="p-2 hover:bg-gray-100 rounded-lg transition-colors">
             <Bell className="h-5 w-5 text-black" />
           </button>
-          <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-            3
-          </span>
+          {/* <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+            
+          </span> */}
         </div>
 
         {/* User dropdown */}
